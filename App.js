5e73c7d3531102d505f54c3ed73081e9fdd2780c
elias <99310710+bo-codes@users.noreply.github.com>
2023-02-18 01:39:19 -0800

    set up simple functionality for TODO creation with non-persistent state/no DB

--- App.js
+++ App.js
@@ -1,20 +1,68 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
+  const [currTodo, setCurrTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const todoInputHandler = (enteredText) => {
+    setCurrTodo(enteredText);
+  };
+
+  const addTodoHandler = () => {
+    // addTodoThunk(currTodo);
+    setTodos((existingTodos) => [...existingTodos, currTodo]);
+    setCurrTodo("")
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="TODO"
+          style={styles.textInput}
+          onChangeText={todoInputHandler}
+          value={currTodo}
+        />
+        <Button title="Create" onPress={addTodoHandler} />
+      </View>
+      <View style={styles.todosList}>
+        <Text>Current TODOS</Text>
+        {todos && todos.map((todo) => {
+          return <Text key={todo}>{todo}</Text>;
+        })}
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "flex-start",
+    paddingTop: 70,
+    paddingHorizontal: 20,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
+    paddingBottom: 30,
+    borderBottomWidth: 0.5,
+    flex: 1,
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "blue",
+    width: "80%",
+    height: 50,
+    marginRight: 8,
+    borderRadius: 8,
+    padding: 8,
+  },
+  todosList: {
+    flex: 6,
   },
 });
